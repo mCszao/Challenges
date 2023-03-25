1fa3da594d13f10ec0ece68a5b07cd8f50d779c0
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-03-25 18:50:28 -0400

    fix organize

---
+++ ListingRestFul/src/shared/components/FlexContainer/flexContainer.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+export const FlexContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-self: flex-end;
+    align-self: flex-end;
+    width: 65vw;
+    border-right: 1px solid gray;
+`;
