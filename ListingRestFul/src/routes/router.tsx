b31edebdba151026fdd051bda6eb1a33cd25c47f
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-03-25 18:52:36 -0400

    fix: organize routes

--- ListingRestFul/src/routes/router.tsx
+++ ListingRestFul/src/routes/router.tsx
@@ -1,9 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import { Feed } from '../pages/Feed/feed';
+import { Comments } from '../pages/Comments/Comments';
+import { Profile } from '../pages/Profile/profile';
 export const Router = () => {
     return (
         <Routes>
             <Route path='/' element={<Feed />} />
+            <Route path='/profile/:slug' element={<Profile />} />
+            <Route path='/post/:slug' element={<Comments />} />
         </Routes>
     );
 };
