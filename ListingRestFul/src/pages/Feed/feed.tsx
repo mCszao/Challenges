ec1dba969382e792d90acbda435198002b9f1784
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-03-26 20:03:57 -0400

    feat: new component to show users

--- ListingRestFul/src/pages/Feed/feed.tsx
+++ ListingRestFul/src/pages/Feed/feed.tsx
@@ -3,12 +3,18 @@ import { useApi } from '../../shared/hooks/useApi';
 import { IPost } from '../../shared/interface/IPost';
 import { IUser } from '../../shared/interface/IUser';
 import { filteredPost } from '../../shared/services/FilteredPost';
-import { StyledPost } from './styles/feed.styled';
+import { StyledPost, StyledResponsiveIcon } from './styles/feed.styled';
 import { FlexContainer } from '../../shared/components/FlexContainer/flexContainer.styled';
+import { BsPeopleFill } from 'react-icons/bs';
+import {
+    BoxResult,
+    StyledItemResult,
+} from '../../shared/components/SideBar/styles/SideBar.styled';
 
 export const Feed = () => {
     const [posts, setPosts] = useState<IPost[]>([]);
     const [users, setUsers] = useState<IUser[]>([]);
+    const [isOpen, setOpen] = useState<boolean>(true);
     useEffect(() => {
         loadInformation();
     }, []);
@@ -25,16 +31,37 @@ export const Feed = () => {
 
     return (
         <FlexContainer>
-            {filteredPosts.map((post, index) => (
-                <StyledPost
-                    key={index}
-                    username={post.name}
-                    title={post.title}
-                    body={post.body}
-                    userId={post.userId}
-                    postId={post.postId}
-                />
-            ))}
+            {isOpen ? (
+                filteredPosts.map((post, index) => (
+                    <StyledPost
+                        key={index}
+                        username={post.name}
+                        title={post.title}
+                        body={post.body}
+                        userId={post.userId}
+                        postId={post.postId}
+                    />
+                ))
+            ) : (
+                <FlexContainer>
+                    <BoxResult>
+                        {users.map((user, index) => (
+                            <StyledItemResult
+                                key={index}
+                                name={user.name}
+                                username={user.username}
+                                id={user.id}
+                            />
+                        ))}
+                    </BoxResult>
+                </FlexContainer>
+            )}
+            <StyledResponsiveIcon
+                isOpen={isOpen}
+                onClick={() => setOpen(!isOpen)}
+            >
+                <BsPeopleFill size={'1rem'} />
+            </StyledResponsiveIcon>
         </FlexContainer>
     );
 };
--- ListingRestFul/src/pages/Feed/styles/feed.styled.ts
+++ ListingRestFul/src/pages/Feed/styles/feed.styled.ts
@@ -1,19 +1,35 @@
 import styled from 'styled-components';
 import { Post } from '../post';
+import { ResponsiveIconProps } from '../types/ResponsiveIconProps';
 
 export const StyledPost = styled(Post)`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    gap: 1rem;
     text-align: justify;
     border-bottom: 1px solid gray;
-    width: 50vw;
-    height: 20rem;
-    max-height: 30vh;
+    width: 60vw;
+    height: 30vh;
+    max-height: 40vh;
     background-color: #fff;
     border: 1px solid gray;
-    padding: 0.5rem;
+
+    padding: 1rem;
+
+    box-sizing: border-box;
+    @media (max-width: 720px) {
+        width: 100vw;
+        display: table;
+
+        border-top: 1px solid gray;
+
+        margin-top: 0;
+    }
+
+    &&:nth-child(1) {
+        border-radius: 1rem 1rem 0 0;
+        margin-top: 2rem;
+    }
 
     &&:hover {
         background-color: #cce9f4;
@@ -26,26 +42,42 @@ export const StyledPost = styled(Post)`
     p {
         margin-bottom: 0.5rem;
     }
-    &&:nth-child(1) {
-        margin-top: 1rem;
-        border-radius: 1rem 1rem 0 0;
-    }
-
     a {
+        margin-top: 1rem;
         text-decoration: none;
         color: #2a86ed;
         cursor: pointer;
-        font-size: 1.5rem;
+        font-size: 1.2rem;
         font-weight: 400;
+        transition: all 0.5s ease;
+        margin-bottom: 1rem;
+
+        height: 2vh;
+    }
+    a:hover {
+        transform: translateY(15%);
     }
 `;
 
 export const LinkCommentary = styled.a`
     align-self: flex-end;
-    transition: all 0.5s ease;
+    margin-bottom: 2rem;
 
     &&:hover {
-        transform: translateY(15%);
         color: #80a7d1;
+    }
+`;
+
+export const StyledResponsiveIcon = styled.div<ResponsiveIconProps>`
+    display: none;
+    padding: 0.5rem;
+    border-radius: 50%;
+    position: fixed;
+    top: 1%;
+    right: 1%;
+    background-color: #80a7d1;
+
+    @media (max-width: 720px) {
+        display: flex;
     }
 `;
